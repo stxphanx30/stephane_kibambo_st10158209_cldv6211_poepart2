1b159f273707e8e4b36ddafdcc55c3f083b2ca3e
lab_services_student <lab_services_student@labVMH8OX>
2024-04-02 19:24:13 +0200

    Add project files.

---
+++ uhj/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace uhj.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
